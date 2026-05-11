d191beebd07c4c4ac19a693e2e91a34bc7b19b6e
oliver6312 <oliver.juhl.05@gmail.com>
2026-05-11 10:25:48 +0200

    update 1.0.0
    fixed the physics on tilemap by adding a paint grid

--- Scenes/protagonist.gd
+++ Scenes/protagonist.gd
@@ -1,25 +1,52 @@
 extends CharacterBody2D
 
+@export var max_speed := 220.0
+@export var ground_accel := 1800.0
+@export var ground_decel := 2200.0
+@export var air_accel := 1200.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var jump_velocity := -420.0
+@export var gravity_up := 900.0
+@export var gravity_down := 1500.0
+@export var jump_cut_multiplier := 0.45
 
+@export var coyote_time := 0.1
+@export var jump_buffer_time := 0.12
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+var coyote_timer := 0.0
+var jump_buffer_timer := 0.0
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func _physics_process(delta):
+	var input_dir := Input.get_axis("move_left", "move_right")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if is_on_floor():
+		coyote_timer = coyote_time
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		coyote_timer -= delta
+
+	if Input.is_action_just_pressed("jump"):
+		jump_buffer_timer = jump_buffer_time
+	else:
+		jump_buffer_timer -= delta
+
+	var accel := ground_accel if is_on_floor() else air_accel
+
+	if input_dir != 0:
+		velocity.x = move_toward(velocity.x, input_dir * max_speed, accel * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, ground_decel * delta)
+
+	if jump_buffer_timer > 0 and coyote_timer > 0:
+		velocity.y = jump_velocity
+		jump_buffer_timer = 0
+		coyote_timer = 0
+
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= jump_cut_multiplier
+
+	if velocity.y < 0:
+		velocity.y += gravity_up * delta
+	else:
+		velocity.y += gravity_down * delta
 
 	move_and_slide()
