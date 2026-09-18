50ca26f953a8867e4d637e982dc2b3b338ec6e62
oliver6312 <oliver.juhl.05@gmail.com>
2026-09-18 17:47:16 +0200

    1.6.0
    Can leave room

---
+++ Scripts/exit_area.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+#@onready var player = %PlayerCharacter
+@export var room = "HomeRoom"
+
+func send_position() -> void:
+	
+	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("player exits room")
+		match room:
+			"HomeRoom":
+				get_tree().change_scene_to_file.call_deferred("res://Scenes/Room/HomeRoom.tscn")
+			"PrototypeRoom":
+				get_tree().change_scene_to_file.call_deferred("res://Scenes/PrototypeLevel.tscn")
+	pass
