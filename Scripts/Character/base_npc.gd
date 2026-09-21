3969876cc5cb08646bfe03247694946f6d269ae9
oliver6312 <oliver.juhl.05@gmail.com>
2026-09-21 21:51:41 +0200

    1.8.1
    added first NPC

---
+++ Scripts/Character/base_npc.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+@export var character: String
+@export var greetings: String
+@export var battler: bool
+
+var player_in_chat_zone = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("attack") and player_in_chat_zone:
+		print("player activates NPC")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_in_chat_zone = true
+		print("player enters NPC area")
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_in_chat_zone = false
+		print("player leaves NPC area")
